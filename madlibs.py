538c7e321760ba76de3ea632f68a38c7f9b02776
Donald Vallejo <donny.vallejo@gmail.com>
2019-08-28 20:58:46 -0700

    testing inputs

--- madlibs.py
+++ madlibs.py
@@ -11,6 +11,49 @@
 # output - print sentence variables. display if it's a verb adjective etc
 # processing - separate variables for each word
 # print whole string. prompt for each blank word. print new string with user's input.
-def main():
-    printEmpty
 
+
+def main():
+    print('test')
+    madlib()
+    adjective = wordAdjective()
+    noun = wordNoun()
+    verb = wordVerb()
+    print("test")
+    return True
+    # printEmpty()
+
+
+def madlib():
+    story = "there was once a _____(adjective) person. who wanted to be a _____(noun). So one day he/she finally got to ______(verb)"
+    print(story)
+    return True
+
+
+def wordAdjective():
+    adjective = ""
+    print(len(adjective))
+    while len(adjective) == 0 or isinstance(adjective, str) == False:
+        adjective = input("there was once a _______(adjective): ")
+        if len(adjective) == 0 or isinstance(adjective, str) == False:
+            print("must input a string with length greater than 0!")
+            
+    return adjective
+
+def wordNoun():
+    noun = ""
+    while len(noun) == 0 or isinstance(noun, str) == False:
+        noun = input("who wanted to be a _____(noun): ")
+        if len(noun) == 0 or isinstance(noun, str) == False:
+            print("must input a string with length greater than 0!")
+    return noun
+
+def wordVerb():
+    verb = ""
+    while len(verb) == 0 or isinstance(verb, str) == False:
+        verb = input("So one day he/she finally got to ______(verb): " )
+        if len(verb) == 0 or isinstance(verb, str) == False:
+            print("must input a string with length greater than 0!")
+    return verb
+
+main()
