e4e435ebd961f7bf665ca78620812a71f75e0f7c
Donald Vallejo <donny.vallejo@gmail.com>
2019-08-28 20:40:44 -0700

    first commit with things

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+# Provide a Mad Libs story for users to fill in
+# Collect user input for the words
+# Print the story with all blanks filled in by the user’s words
+# Use at least 3 parts of speech for word replacement
+# Use variable assignment to store/use values
+# Write and call functions
+# Use at least one of the following core data types: strings, integers, floats
+# Use at least one of the following collection types: lists, tuples, dictionary
+
+# input - hardcode whole madlib, have variables for each blank word
+# output - print sentence variables. display if it's a verb adjective etc
+# processing - separate variables for each word
+# print whole string. prompt for each blank word. print new string with user's input.
+def main():
+    printEmpty
+
